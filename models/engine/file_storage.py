5dbb387325a68eb4ddd0b87a075994818c58c705
Barnabas27 <bnyambura032@gmail.com>
2021-11-11 23:22:58 +0300

    add Basemodel

---
+++ models/engine/file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Class FileStorage serializes instances to a JSON
+file and deserializes JSON file to instances:
+"""
+
+import models
+from models.base_model import BaseModel
+
