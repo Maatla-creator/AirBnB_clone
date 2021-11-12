6d68ea27304e2daf31b16319d1aa9816ed0ecb27
Barnabas27 <bnyambura032@gmail.com>
2021-11-12 15:43:02 +0300

    add instances of Basemodel

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,9 @@ Class FileStorage serializes instances to a JSON
 file and deserializes JSON file to instances:
 """
 
+import sys, json
 import models
+import os.path as path
 from models.base_model import BaseModel
 
 class FileStorage:
@@ -25,11 +27,21 @@ class FileStorage:
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
 
-        pass
+        new_dictionary = {}
+        with open(self.__file_path, mode="w", encoding='UTF-8') as f:
+            for k, v in self.__objects.items():
+                new_dictionary[k] = v.to_dict()
+            json.dump(new_dictionary, f)
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file
         (__file_path) exists ; otherwise, do nothing. If the file doesn’t
         exist, no exception should be raised)"""
 
-        pass
+        if path.isfile(self.__file_path):
+            with open(self.__file_path, mode="r", encoding='UTF-8') as f:
+                for k, v in (json.load(f)).items():
+                    v = eval(v["__class__"])(**v)
+                    self.__objects[k] = v
+
+        
